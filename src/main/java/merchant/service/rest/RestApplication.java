9483c6f9463a7cb8bd5c96eea8c2f30ea42de828
Andreas Birch <andreasbirch1998@hotmail.com>
2024-01-19 12:58:08 +0100

    Revert "Delete RestApplication.java"
    This reverts commit 649e0f14c3dfce06f55e3e11629497c508418210.

---
+++ src/main/java/merchant/service/rest/RestApplication.java
@@ -0,0 +1,10 @@
+package merchant.service.rest;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
